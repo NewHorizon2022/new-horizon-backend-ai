b9334d36bd9ff9dcf1f71f40d6c4701543079b11
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2022-11-18 02:54:14 +0100

    adding some logs

--- src/shared_services/image_processing_service.py
+++ src/shared_services/image_processing_service.py
@@ -32,16 +32,22 @@ def process_image(detected_faces, image_url, file_name):
     img = Image.open(BytesIO(response.content))
 
     # For each face returned use the face rectangle and draw a box.
-    print('Drawing rectangle around face... see popup for results.')
+    logging.info(('Drawing rectangle around face... see popup for results.')
     draw = ImageDraw.Draw(img)
     for face in detected_faces:
         rectangle = getRectangle(face)
         draw.rounded_rectangle(rectangle, outline='blue', width=5)
 
     # Add the count of faces detected to the image
-    fnt = ImageFont.truetype("sans-serif.ttf", 60)
+    logging.info('loading font')
+    fnt = ImageFont.truetype("/usr/share/fonts/truetype/Sarai/Sarai.ttf", 60)
+    logging.info(f'done loading font. Image size {img.size}')
+
     width, height = img.size
+    logging.info(f'width: {width}, height: {height}')
+
     draw.text((width / 2, height / 2), detected_faces.__len__(), font=fnt, fill=(107, 0, 255, 64))
+    logging.info('done drawing text')
 
     # Display the image in the users default image browser.
     logging.info(f'Saving {file_name}...')
